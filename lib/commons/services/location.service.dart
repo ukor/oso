afd300c52e04b101fe5145830871e70c8170e1b6
Jidechi ukor <10085056+ukor@users.noreply.github.com>
2025-05-20 13:22:03 +0100

    improved location tracking

---
+++ lib/commons/services/location.service.dart
@@ -0,0 +1,197 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:geolocator/geolocator.dart';
+import 'package:logger/logger.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LocationService {
+  final _log = Logger();
+
+  late double latitude;
+  late double longitude;
+
+  String? currentAddress;
+
+  bool isLocationEnabled = false;
+
+  getLastKnowLocation() async {
+    await Geolocator.getLastKnownPosition(forceAndroidLocationManager: true);
+  }
+
+  LocationSettings _locationSettings() {
+    if (Platform.isAndroid) {
+      return AndroidSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+        forceLocationManager: true,
+        intervalDuration: const Duration(minutes: 5),
+        //(Optional) Set foreground notification config to keep the app alive
+        //when going to the background
+        foregroundNotificationConfig: const ForegroundNotificationConfig(
+          notificationText:
+              "Oso will continue to receive your location even when you aren't using it",
+          notificationTitle: "Oso is running",
+          enableWakeLock: true,
+        ),
+      );
+    }
+
+    return AppleSettings(
+      accuracy: LocationAccuracy.high,
+      activityType: ActivityType.fitness,
+      distanceFilter: 100,
+      pauseLocationUpdatesAutomatically: false,
+      // Only set to true if our app will be started up in the background.
+      showBackgroundLocationIndicator: true,
+      allowBackgroundLocationUpdates: true,
+    );
+  }
+
+  Future<Position?> getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        // desiredAccuracy: LocationAccuracy.high,
+        desiredAccuracy: LocationAccuracy.bestForNavigation,
+        forceAndroidLocationManager: true,
+        timeLimit: const Duration(seconds: 10),
+      );
+
+      return position;
+    } catch (e) {
+      final position = await Geolocator.getLastKnownPosition();
+
+      return position;
+    }
+  }
+
+  Stream<ServiceStatus> getLocationPermissionStatusContinuously() {
+    Stream<ServiceStatus> statusStream = Geolocator.getServiceStatusStream();
+
+    return statusStream;
+  }
+
+  ///
+  /// Improved from my original idea without rxDart
+  /// Stream<Position>? positionStream;
+  ///
+  ///     final statusSubscription = statusStream.listen((ServiceStatus v) {
+  ///       if (v == ServiceStatus.enabled && positionStream == null) {
+  ///         positionStream = Geolocator.getPositionStream(
+  ///           locationSettings: _locationSettings(),
+  ///         );
+  ///       } else if (v == ServiceStatus.disabled && positionStream != null) {
+  ///         positionStream = null;
+  ///       }
+  ///     });
+  ///
+  Stream<Position> getLocationContinuously() {
+    final controller = StreamController<Position>.broadcast();
+    StreamSubscription<ServiceStatus>? statusSubscription;
+    StreamSubscription<Position>? positionSubscription;
+    bool isDisposed = false;
+
+    void startPositionStream() {
+      if (isDisposed) return; // Don't start if already disposed
+      positionSubscription = Geolocator.getPositionStream(
+        locationSettings: _locationSettings(),
+      ).listen(
+        (position) {
+          if (!controller.isClosed) {
+            controller.add(position);
+          }
+        },
+        onError: (error) {
+          if (!controller.isClosed) {
+            controller.addError(error);
+          }
+        },
+        onDone: () {
+          if (!controller.isClosed) {
+            //  controller.close(); // Don't close here, status stream controls this.
+          }
+        },
+      );
+    }
+
+    void stopPositionStream() {
+      positionSubscription?.cancel();
+      positionSubscription = null;
+    }
+
+    // Listen to service status changes.
+    statusSubscription = Geolocator.getServiceStatusStream().listen(
+      (status) {
+        if (isDisposed) return; // Don't react if disposed
+        if (status == ServiceStatus.enabled) {
+          startPositionStream();
+        } else {
+          stopPositionStream();
+
+          if (!controller.isClosed) {
+            controller.addError(StateError('Location service is disabled.'));
+          }
+        }
+      },
+      onError: (error) {
+        if (!controller.isClosed) {
+          controller.addError(error);
+        }
+      },
+      onDone: () {
+        if (!controller.isClosed) {
+          controller.close();
+        }
+      },
+    );
+
+    // Check the initial service status.  Crucial!
+    Geolocator.isLocationServiceEnabled()
+        .then((enabled) {
+          if (isDisposed) return; // Don't react if disposed
+          if (enabled) {
+            startPositionStream();
+          } else {
+            stopPositionStream();
+            if (!controller.isClosed) {
+              controller.addError(
+                StateError('Location service is initially disabled.'),
+              );
+            }
+          }
+        })
+        .catchError((error) {
+          if (!controller.isClosed) {
+            controller.addError(error);
+          }
+        });
+
+    // Ensure proper cleanup.  Use controller.close()
+    controller.onCancel = () {
+      isDisposed = true;
+      statusSubscription?.cancel();
+      stopPositionStream();
+      // controller.close();  // Close only once.  Moved to statusStream onDone and initial check
+    };
+
+    return controller.stream;
+  }
+
+  Stream<Position> getLocationContinuouslyV2() {
+    Stream<ServiceStatus> statusStream =
+        Geolocator.getServiceStatusStream().distinct();
+
+    return statusStream.switchMap((ServiceStatus status) {
+      if (status == ServiceStatus.enabled) {
+        return Geolocator.getPositionStream(
+          locationSettings: _locationSettings(),
+        ).handleError((error, stackTrace) {
+          _log.f(error, stackTrace: stackTrace);
+          return const Stream.empty();
+        });
+      } else {
+        return const Stream.empty();
+      }
+    });
+  }
+}
--- lib/commons/services/permission.service.dart
+++ lib/commons/services/permission.service.dart
@@ -1,22 +1,46 @@
 import 'package:logger/logger.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-enum LocationPermissionStatus { granted, notGranted }
+enum AppPermissionStatus { granted, notGranted }
 
-class LocationPermissionService {
+class PermissionService {
   final _log = Logger();
 
-  LocationPermissionStatus _isGrated(PermissionStatus ps) {
-    final isGrated = ps.isGranted;
-    if (isGrated) {
-      return LocationPermissionStatus.granted;
-    }
+  Future<void> requestForAllNecessaryPermissions() async {
+    await requestNotificationPermission();
 
-    return LocationPermissionStatus.notGranted;
+    await requestLocationPermission();
   }
 
-  Future<LocationPermissionStatus> getPermission() async {
-    final locationPermission = Permission.location;
+  AppPermissionStatus _isGrated(PermissionStatus ps) {
+    final isGrated = ps.isGranted;
+    if (isGrated) {
+      return AppPermissionStatus.granted;
+    }
+
+    return AppPermissionStatus.notGranted;
+  }
+
+  Future<bool> isNotificationPermissionGranted() async {
+    const permission = Permission.location;
+
+    final status = await permission.status;
+
+    return status.isGranted;
+  }
+
+  Future<bool> isLocationPermissionGranted() async {
+    const locationPermission = Permission.location;
+
+    final status = await locationPermission.status;
+
+    return status.isGranted;
+  }
+
+  Future<AppPermissionStatus> requestLocationPermission() async {
+    const locationPermission = Permission.location;
+
+    await locationPermission.shouldShowRequestRationale;
 
     final status = await locationPermission.status;
 
@@ -53,6 +77,52 @@ class LocationPermissionService {
     }
 
     _log.d('end ${status.name}');
+
+    return _isGrated(status);
+  }
+
+  Future<AppPermissionStatus> requestNotificationPermission() async {
+    const permission = Permission.notification;
+
+    await permission.shouldShowRequestRationale;
+
+    final status = await permission.status;
+
+    _log.d("notification ${status.name}");
+
+    if (status.isPermanentlyDenied) {
+      final opened = await openAppSettings();
+    }
+
+    if (status.isDenied) {
+      // We haven't asked for permission yet or the permission has been denied before, but not permanently.
+      final request = await permission.request();
+
+      _log.d('notification request from denied ${request.name}');
+
+      if (request.isPermanentlyDenied) {
+        final opened = await openAppSettings();
+        _log.d('notification open app settings in denied $opened');
+      }
+
+      _log.d(
+        'notification request from denied after open app settings ${request.name}',
+      );
+
+      return _isGrated(request);
+    }
+
+    // You can also directly ask permission about its status.
+    if (status.isRestricted) {
+      // The OS restricts access, for example, because of parental controls.
+      final request = await permission.request();
+
+      _log.d('notification request from restricted ${request.name}');
+
+      return _isGrated(request);
+    }
+
+    _log.d('notification permission request end ${status.name}');
 
     return _isGrated(status);
   }
